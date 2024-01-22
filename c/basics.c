59b1b49ba7b7f78c4f0f466362f7ded354589039
Muriel <2029chan.muriel@fcboe.org>
2024-01-22 15:41:10 -0500

    added basics.c

---
+++ c/basics.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main ()
+{
+	printf("This is my c basics program. \n");
+	int num;
+	printf("Enter a number: ");
+	scanf("%i", &num);	
+	printf("You entered %i\n", num);
+	return 0;
+}
+
